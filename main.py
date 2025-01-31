9ceaca0ea44e916621079485071de2b571c0673f
OKAMOTO Shigehiro <primary@oka4shi.dev>
2025-02-01 05:42:10 +0900

    remove unused codes

--- main.py
+++ main.py
@@ -4,15 +4,7 @@ import os
 from googleapiclient.discovery import build
 
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
-
 def get_youtube_client(api_key: str):
-    # Get credentials and create an API client
-    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-    #     client_secrets_file, scopes)
-    # credentials = flow.run_console()
-
     return build("youtube", "v3", developerKey=api_key)
 
 
